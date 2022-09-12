2f6eb1319ee589588120d5150d4732c717ffbeb3
cristian <u20201b845@upc.edu.pe>
2022-09-11 22:00:40 -0500

    add page Contenido.html

---
+++ app.js
@@ -0,0 +1,18 @@
+const product="tele, ARRROZ, adios ,pollo";
+
+const objeto ={
+    nombre:"cristian",
+    edad:20,
+    status:"baja",
+    hobies:["dormir","jugar"],
+     Licencia:{
+        moto:"A1",
+        carro:"b2b"
+    }
+}
+
+console.log(objeto);
+//console.log(product.split(","));
+//console.log(product);
+//console.log(product.toUpperCase());
+//console.log(product.toLowerCase());
